cb750a5135025fe52955a5f084866317f029a084
gausszh <gauss.zh@gmail.com>
2015-02-07 15:16:00 +0800

    初始化

---
+++ blog/site_settings.py
@@ -0,0 +1,2 @@
+SECRET_KEY = 'w3kiuq%^)9joh(slzp13_1krx9(garj3a@_9yft@rl1+3^p!8h'
+SITE_ID = 1
---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'blog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^weblog/', include('zinnia.urls', namespace='zinnia')),
+    url(r'^comments/', include('django_comments.urls')),
+)
