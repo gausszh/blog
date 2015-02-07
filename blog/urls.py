56ba9b6a6de1b9ef4534b223d4d4250b59accc85
gausszh <gauss.zh@gmail.com>
2015-02-07 15:57:49 +0800

    本地化

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'blog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^', include('zinnia.urls', namespace='zinnia')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^weblog/', include('zinnia.urls', namespace='zinnia')),
     url(r'^comments/', include('django_comments.urls')),
